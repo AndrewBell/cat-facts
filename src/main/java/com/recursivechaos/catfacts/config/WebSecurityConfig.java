d2bf31e8c22808f9447f9f6825d0a0ec7186b0bf
Andrew Bell <andrew@recursivechaos.com>
2016-04-26 18:38:24 -0500

    Secures actuator endpoints, and permits anonymous access to resource endpoints

---
+++ src/main/java/com/recursivechaos/catfacts/config/WebSecurityConfig.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Andrew Bell 4/26/2016
+ * www.recursivechaos.com
+ * andrew@recursivechaos.com
+ * Licensed under MIT License 2016. See license.txt for details.
+ */
+
+package com.recursivechaos.catfacts.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/catfacts/**").permitAll()
+            .anyRequest().authenticated()
+        .and().csrf().disable();
+    }
+}
